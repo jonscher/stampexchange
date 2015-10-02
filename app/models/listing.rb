58a0a9902abc5640bb4604d0c5f01deb111f06ba
Jonathan <jonscher@hotmail.com>
2015-10-02 22:12:43 +0200

    'ok'

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,6 @@
 class Listing < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
+  validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)'
   validates :name, :price, :description, presence: true
   validates :price, numericality: {greater_than:0}
 
