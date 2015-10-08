02838478289453284d4ee83a4318d74d9657a2ff
Jonathan <jonscher@hotmail.com>
2015-10-08 18:10:33 +0200

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery_ujs
+
 //= require bootstrap
+//= require jquery_ujs
 // = require_tree .
 
