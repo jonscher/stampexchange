f9d8a15e772a5528b0715f4dcca7749e17204b2b
Jonathan <jonscher@hotmail.com>
2015-10-10 11:38:29 +0200

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,7 @@
 // about supported directives.
 //= require jquery_ujs
 //= require bootstrap
+//= require bootstrap/modal
+//= require bootstrap/dropdown
 //= require twitter/bootstrap/rails/confirm
 // = require_tree .
