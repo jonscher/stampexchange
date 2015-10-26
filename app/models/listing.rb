bec7ca68bf21a4767c74bf116fbb5cd2f1a7eace
Jonathan <jonscher@hotmail.com>
2015-10-26 22:59:25 +0100

    ok

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,8 @@
 class Listing < ActiveRecord::Base
 if Rails.env.development?
-  has_attached_file :image, styles: { medium: "300x", thumb: "200x" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }, default_url: "/images/:style/missing.png"
 else
-   has_attached_file :image, styles: { medium: "300x", thumb: "200x" }, default_url: "/images/:style/missing.png",
+   has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }, default_url: "/images/:style/missing.png",
 
       :storage => :dropbox,
       :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
@@ -10,7 +10,7 @@ else
 end
 
   validates_attachment_size :image, :less_than => 2.megabytes
-  validates_attachment_content_type :image, styles: {medium: "300x", thumb: "200x"}, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_content_type :image, styles: {medium: "300x300", thumb: "100x100"}, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates :name, :price, :description, :image, presence: true
   validates :price, numericality: {greater_than:0}
   belongs_to :user
