27de0e69dadc5c3460c82ba1454e1e46fe566359
Jonathan <jonscher@hotmail.com>
2015-10-27 11:10:40 +0100

    ok

--- db/migrate/20151001163819_add_attachment_image_to_listings.rb
+++ db/migrate/20151001163819_add_attachment_image_to_listings.rb
@@ -1,8 +1,9 @@
 class AddAttachmentImageToListings < ActiveRecord::Migration
   def self.up
     change_table :listings do |t|
-      t.attachment :image
-  
+      t.attachment :image1
+      t.attachment :image2
+      t.attachment :image3
     end
   end
 
